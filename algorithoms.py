0b0d16b9243418c72019708036602a76899d24e8
naahian <nahiandev@gmail.com>
2024-10-19 21:40:20 +0600

    added a_star algo

--- algorithoms.py
+++ algorithoms.py
@@ -1,4 +1,4 @@
-from constants import Config
+from queue import PriorityQueue
 from tile import Tile
 
 
@@ -20,11 +20,11 @@ class Algorithoms:
           
     def dfs(self, start:Tile, end:Tile):
         self.path = [] #clear previous data
-        visited = [[0 for i in range(len(self.graph))] for j in range(len(self.graph))]
-        parents = [[0 for i in range(len(self.graph))] for j in range(len(self.graph))]
+        visited = {tile:False for row in self.graph for tile in row}
+        parents = {tile:None for row in self.graph for tile in row}
         stack = []
         stack.append(start)
-        visited[start.col][start.row] = 1
+        visited[start] = True
 
         while(len(stack)>0):
             tile = stack.pop()
@@ -33,10 +33,9 @@ class Algorithoms:
             self._drawExplored(tile)
 
             for nbr in tile.neighbors:
-                y, x = nbr.col, nbr.row
-                if(visited[y][x] == 0 and not nbr.isObstacle()):
-                    visited[y][x] = 1
-                    parents[y][x] = tile
+                if(not visited[nbr] and not nbr.isObstacle()):
+                    visited[nbr] = True
+                    parents[nbr] = tile
                     stack.append(nbr)
         self._backtrack(parents, start, end)
         self._drawPath(start)
@@ -44,9 +43,10 @@ class Algorithoms:
     def bfs(self, start:Tile, end:Tile):
         self.path = []  #clear previous data
         queue = []
-        visited = [[0 for i in range(len(self.graph))] for j in range(len(self.graph))]
-        parents = [[0 for i in range(len(self.graph))] for j in range(len(self.graph))]
+        visited = {tile:False for row in self.graph for tile in row}
+        parents = {tile:None for row in self.graph for tile in row}
         queue.append(start)
+        visited[start] = True
         
         while(len(queue)>0):
             tile = queue.pop(0)
@@ -54,21 +54,20 @@ class Algorithoms:
             self._drawExplored(tile)
 
             for nbr in tile.neighbors:
-                y,x = nbr.col, nbr.row
-                if(visited[y][x] == 0 and not nbr.isObstacle()):
-                    visited[y][x] = 1
-                    parents[y][x] = tile
+                if(not visited[nbr] and not nbr.isObstacle()):
+                    visited[nbr] = True
+                    parents[nbr] = tile
                     queue.append(nbr)
 
-        self._backtrack(parents, start, end)
+        self._backtrack(parents, start, end) 
         self._drawPath(start)
 
     #output same as bfs since undirected graph
     def dijkstra(self ,start:Tile, end:Tile):
         self.path = []  #clear previous data
         queue = []
-        parents = [[0 for i in range(len(self.graph))] for j in range(len(self.graph))]
-        distance = [[Config.rows**2 for i in range(len(self.graph))] for j in range(len(self.graph))]
+        parents = {tile:None for row in self.graph for tile in row}
+        distance = [[float('inf') for i in range(len(self.graph))] for j in range(len(self.graph))]
         queue.append(start)
         distance[start.col][start.row] = 0
 
@@ -84,20 +83,73 @@ class Algorithoms:
 
                 if(tempDist < distance[y][x] and not nbr.isObstacle()):
                     distance[y][x] = tempDist
-                    parents[y][x] = tile
+                    parents[nbr] = tile
                     queue.append(nbr)
 
         self._backtrack(parents, start, end)
         self._drawPath(start)
 
+
+
     def a_star(self, start:Tile, end:Tile):
-        pass
+        self.path = []  #clear previous data
+        pqueue = [] 
+        g_score = {tile:float('inf') for row in self.graph for tile in row}
+        f_score = {tile:float('inf') for row in self.graph for tile in row}
+        visited = {tile:False for row in self.graph for tile in row}
+        parents = {tile:None for row in self.graph for tile in row}
+
+        pqueue.append((f_score[start], start)) #(f_score, tile)
+        visited[start] = True
+        g_score[start] = 0
+        f_score[start] = self.h_score(start, end)
+
+        while(len(pqueue) > 0):
+            tileData = self.pop_min(pqueue, 0)
+            tile = tileData[1]
+            self._drawExplored(tile)            
+            if(tile == end): break
+
+            for nbr in tile.neighbors:
+                if(not visited[nbr] and not nbr.isObstacle()):
+                    visited[nbr] = True
+                    parents[nbr] = tile
+                    g_score[nbr] = g_score[tile] + 1
+                    h = self.h_score(nbr, end)
+                    f_score[nbr] = g_score[nbr] + h
+                    pqueue.append((f_score[nbr], nbr))
+        
+        self._backtrack(parents, start, end)
+        self._drawPath(start)
+
+
+
+    def pop_min(self, queue:list, index):
+        min = queue[0]
+        for item in queue:
+            if(item[index] < min[index]): min = item
+        queue.remove(min)
+        return min
+    
+    #heuristic
+    def h_score(self, current:Tile, end:Tile):
+        return abs((current.row - end.row)) + abs((current.col - end.col))
 
     #to find shortest path
     def _backtrack(self, parents, start, end):
         while True:
-            end = parents[end.col][end.row]
-            if(end==0): return
+            end = parents[end]
+            if(end==None): return
             self.path.append(end)
             if(start==end):break
 
+# class T:
+#     def __init__(self) -> None:
+#         pass
+
+# arr = PriorityQueue()
+# arr.put((T(),3))
+# arr.put((T(),1))
+# arr.put((T(),2))
+
+# print(arr.get()[0])
